cb0a6b448308b0a6c462842a4f383a60997f6ff7
GalinaKr <kravchukgi@gmail.com>
2021-10-20 10:33:33 +0300

    add UUID into DeliveryManager

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_20_062327) do
+ActiveRecord::Schema.define(version: 2021_10_20_073126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -49,7 +49,8 @@ ActiveRecord::Schema.define(version: 2021_10_20_062327) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "delivery_managers", force: :cascade do |t|
+  create_table "delivery_managers", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.bigint "integer_id"
     t.string "enabled"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
