1e6b85f57024053bf29be5fa50c910b1d82e171f
Galka <37017836+GalinaKr@users.noreply.github.com>
2021-10-25 19:50:32 +0300

    Us 5 PackageAssignment (#5)
    Package Assignment

---
+++ db/migrate/20211020131713_create_package_assignments.rb
@@ -0,0 +1,10 @@
+class CreatePackageAssignments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :package_assignments do |t|
+      t.references :courier, null: false, foreign_key: true
+      t.references :package, null: false, foreign_key: true, type: :uuid
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/package_assignments.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :package_assignment do
+    
+  end
+end
